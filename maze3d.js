e97c70661cb36c607fca48609f5c323b7c80bd06
Kirill Shiryaev <infantem.help@gmail.com>
2022-09-18 00:30:27 +0300

    string

--- maze3d.js
+++ maze3d.js
@@ -30,13 +30,12 @@ class Maze3d {
     for (let i = 0; i < this.dimensions; i++) {
       total += `Level ${i}\n`
       let table = '\t';
-      table += "-".repeat(this.columns * 3) + "\n";
+      table += " " + "---".repeat(this.columns + 1) + "\n";
       for (let j = 0; j < this.rows; j++) {
-        let row = "\t";
+        let row = "\t|";
         table += '\t'
         for (let k = 0; k < this.columns; k++) {
           const cell = this.maze[i][j][k];
-          const rightCell = k + 1 < this.columns ? this.maze[i][j][k + 1] : false;
           if (cell.left || k === 0) {
             table += "|"
           } else {
@@ -44,36 +43,34 @@ class Maze3d {
           };
 
           if (i === this.start[0] && j === this.start[1] && k === this.start[2]) {
-            table += "S";
+            table += " S ";
           } else if (i === this.goal[0] && j === this.goal[1] && k === this.goal[2]) {
-            table += "G";
+            table += " G ";
           } else if (!cell.up && !cell.down) {
-            table += "\u2b65"
+            table += " \u2b65 "
           } else if (!cell.up) {
-            table += '\u2191'
+            table += ' \u2191 '
           } else if (!cell.down) {
-            table += "\u2193"
+            table += " \u2193 "
           } else if (cell.up && cell.down) {
-            table += " "
+            table += "   "
           };
 
           if (k === this.columns - 1) {
             table += "|"
-          } else {
-            table += " "
           };
 
           if (!cell.forward) {
-            row += "+++"
+            row += "   +"
           } else {
-            row += "---"
+            row += " - +"
           };
         }
         row = j < (this.rows - 1) ? row + "\n" : ''
         table += "\n" + row
 
       }
-      table += "\t" + "-".repeat(this.columns * 3) + "\n";
+      table += " \t " + "---".repeat(this.columns + 1) + "\n";
       total += table + "\n"
       table = "\t"
     };
