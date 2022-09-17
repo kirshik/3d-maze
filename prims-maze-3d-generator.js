1ebfa138d9082f1a6e2571435b932e171925b8dd
Kirill Shiryaev <infantem.help@gmail.com>
2022-09-17 21:18:27 +0300

    prims alg

---
+++ prims-maze-3d-generator.js
@@ -0,0 +1,9 @@
+//Prim's algorithm (also known as Jarník's algorithm) is a greedy algorithm that finds a minimum spanning tree for a weighted undirected graph.
+
+class PrimsMaze3dGenerator {
+  constructor(maze) {
+    super();
+    this.maze = maze;
+  };
+  generate() { };
+}
