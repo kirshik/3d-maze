0b52e6aeb0d528cc60c6660d418c4a845e56a2e6
Kirill Shiryaev <infantem.help@gmail.com>
2022-09-18 15:02:33 +0300

    dfs

--- dfc-maze-3d-generator.js
+++ dfc-maze-3d-generator.js
@@ -10,7 +10,8 @@ class DFSMaze3dGenerator extends Maze3dGenerator {
       return Math.floor(Math.random() * max);
     }
     function isUnvisitedNeighbours(cell, maze) {
-      for (const direction of directions.values()) {
+      let neighbours = [];
+      for (const [key, direction] of directions.entries()) {
         const conditions = [
           (cell.place[0] + direction[0] < maze.dimensions),
           (cell.place[0] + direction[0] >= 0),
@@ -22,11 +23,11 @@ class DFSMaze3dGenerator extends Maze3dGenerator {
         if (!conditions.includes(false)) {
           const neighbour = maze.maze[cell.place[0] + direction[0]][cell.place[1] + direction[1]][cell.place[2] + direction[2]];
           if (!visited.includes(neighbour)) {
-            return true;
+            neighbours.set(key, neighbour)
           }
         }
       }
-      return false;
+      return neighbours;
     }
     let directions = new Map([
       ["up", [1, 0, 0]],
@@ -45,63 +46,51 @@ class DFSMaze3dGenerator extends Maze3dGenerator {
     stack.push(currentCell);
     visited.push(currentCell);
     while (stack.length > 0) {
-      if (isUnvisitedNeighbours(currentCell, this.maze)) {
-        for (const [key, direction] of directions.entries()) {
-          const conditions = [
-            currentCell.place[0] + direction[0] < this.maze.dimensions,
-            currentCell.place[0] + direction[0] >= 0,
-            currentCell.place[1] + direction[1] < this.maze.rows,
-            currentCell.place[1] + direction[1] >= 0,
-            currentCell.place[2] + direction[2] < this.maze.columns,
-            currentCell.place[2] + direction[2] >= 0
-          ]
-          if (!conditions.includes(false)) {
-            const neighbour = this.maze.maze[currentCell.place[0] + direction[0]][currentCell.place[1] + direction[1]][currentCell.place[2] + direction[2]];
-            if (!visited.includes(neighbour)) {
-              switch (key) {
-                case "up":
-                  currentCell.up = 0;
-                  neighbour.down = 0;
-                  break;
-                case "down":
-                  currentCell.down = 0;
-                  neighbour.up = 0;
-                  break;
-                case "forward":
-                  currentCell.forward = 0;
-                  neighbour.backward = 0;
-                  break;
-                case "backward":
-                  currentCell.backward = 0;
-                  neighbour.forward = 0;
-                  break;
-                case "right":
-                  currentCell.right = 0;
-                  neighbour.left = 0;
-                  break
-                case "left":
-                  currentCell.left = 0;
-                  neighbour.right = 0;
-                  break;
-              }
-              visited.push(neighbour);
-              stack.push(neighbour);
-              currentCell = neighbour;
+      const unvisitedNeighbours = isUnvisitedNeighbours(currentCell, this.maze);
+      if (unvisitedNeighbours.length > 0) {
+        for (const [key, neighbour] of unvisitedNeighbours.entries()) {
+          switch (key) {
+            case "up":
+              currentCell.up = 0;
+              neighbour.down = 0;
               break;
-            }
+            case "down":
+              currentCell.down = 0;
+              neighbour.up = 0;
+              break;
+            case "forward":
+              currentCell.forward = 0;
+              neighbour.backward = 0;
+              break;
+            case "backward":
+              currentCell.backward = 0;
+              neighbour.forward = 0;
+              break;
+            case "right":
+              currentCell.right = 0;
+              neighbour.left = 0;
+              break
+            case "left":
+              currentCell.left = 0;
+              neighbour.right = 0;
+              break;
           }
+          visited.push(neighbour);
+          stack.push(neighbour);
+          currentCell = neighbour;
+          break;
         }
-
       } else {
         currentCell = stack.pop();
       }
-
+      this.maze.goal = visited.pop().place;
+      return this.maze;
     }
-    this.maze.goal = visited.pop().place;
-    return this.maze;
-
 
   }
 
+
 }
+
+
 export default DFSMaze3dGenerator;
