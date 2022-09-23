a29543a37db0c3203ded4184e0f852153f8063c0
Kirill Shiryaev <infantem.help@gmail.com>
2022-09-23 03:20:43 +0300

    search demo

--- main.js
+++ main.js
@@ -1,34 +1,13 @@
-import DFSMaze3dGenerator from "./dfs-maze-3d-generator.js";
-import Maze3dAdapter from "./maze-3d-adapter.js";
-import Maze3d from "./maze3d.js";
-import PrimsMaze3dGenerator from "./prims-maze-3d-generator.js";
-import AStar from "./search-algorithms/a-star-algorithm.js";
-import BFS from "./search-algorithms/breadth-first-search.js";
-import DFS from "./search-algorithms/depth-first-search-algorithm.js";
-import SimpleMaze3dGenerator from "./simple-maze-3d-generator.js";
+import SearchDemo from "./search-demo.js";
 
-
-let m1 = new Maze3d(25, 25, 3);
-
-
-let dfs = new DFSMaze3dGenerator(m1);
-const maze = dfs.generate();
-console.log(maze.toString());
-
-
-const adapter = new Maze3dAdapter(maze);
-
-const astar = new AStar();
-console.log(astar.search(adapter));
-console.log(astar.getNumberOfNodesEvaluated());
-
-const bfs = new BFS();
-bfs.search(adapter);
-console.log(bfs.getNumberOfNodesEvaluated());
-
-const dfsObj = new DFS();
-dfsObj.search(adapter);
-console.log(dfsObj.getNumberOfNodesEvaluated());
+/**
+  result example:
+    BFS: 224
+    DFS: 24996
+    A*: 219
+ */
+let searchDemo = new SearchDemo();
+console.log(searchDemo.run(50, 50, 5))
 
 
 
---
+++ search-demo.js
@@ -0,0 +1,29 @@
+
+import Maze3d from "./maze3d.js";
+import Maze3dAdapter from "./maze-3d-adapter.js"
+import DFSMaze3dGenerator from "./dfs-maze-3d-generator.js";
+import AStar from "./search-algorithms/a-star-algorithm.js";
+import BFS from "./search-algorithms/breadth-first-search.js";
+import DFS from "./search-algorithms/depth-first-search-algorithm.js";
+
+class SearchDemo {
+  constructor() { };
+  #testSearchAlgorithm(searchAlgo, searchable) {
+    const solution = searchAlgo.search(searchable);
+    const numOfNodes = searchAlgo.getNumberOfNodesEvaluated();
+    return numOfNodes;
+  }
+  run(mazeRows, mazeColumns, mazeDimensions) {
+    let maze = new Maze3d(mazeRows, mazeColumns, mazeDimensions);
+    let dfs = new DFSMaze3dGenerator(maze);
+    const generatedMaze = dfs.generate();
+    const adapter = new Maze3dAdapter(generatedMaze);
+
+    const bfsSearch = this.#testSearchAlgorithm(new BFS(), adapter);
+    const dfsSearch = this.#testSearchAlgorithm(new DFS(), adapter);
+    const astarSearch = this.#testSearchAlgorithm(new AStar(), adapter);
+
+    return ` BFS: ${bfsSearch}\n DFS: ${dfsSearch}\n A*: ${astarSearch}\n`
+  }
+}
+export default SearchDemo;
